01bf41b4f71e74476fac55aaf6de846a14aa1fa0
wentin <zhangwenting111@gmail.com>
2014-12-02 19:22:23 -0500

    works on chrome, break line solved

--- js/underline.js
+++ js/underline.js
@@ -4,55 +4,78 @@ function drawText(canvas, text){
 	var canvas = canvas;
 	var ctx = canvas.getContext('2d');
 
-	// canvas.width = canvas.offsetWidth + 5;
-	// canvas.height = canvas.offsetHeight;
+
+
+	ctx.font = '18px Georgia';
+    var metrics = ctx.measureText(text);
+    canvas.width = metrics['width'];
+	canvas.height = canvas.offsetHeight;
+
 	ctx.strokeStyle = '#666666';
     ctx.lineWidth = 0.5;
     var posY = 19.5;
-    console.log(posY);
+
 	ctx.moveTo(0, posY);
 	ctx.lineTo(canvas.width, posY);
 	ctx.stroke();
 
 	ctx.globalCompositeOperation = "destination-out";
-	ctx.font         = '18px Georgia';
-	ctx.fillStyle = '#666666';
+	ctx.font = '18px Georgia';
+	ctx.fillStyle = 'orangered';
 	ctx.textBaseline = 'top';
-	ctx.fillText  (text, -0.5, 0);
-	ctx.fillText  (text, -1, 0);
-	ctx.fillText  (text, -1.5, 0);
-	ctx.fillText  (text, -2, 0);
+	ctx.fillText(text, -0.5, 0);
+	ctx.fillText(text, -1, 0);
+	ctx.fillText(text, -1.5, 0);
+	ctx.fillText(text, -2, 0);
+	ctx.fillText(text, -2.5, 0);
 
-	ctx.font         = '18px Georgia';
-	ctx.fillStyle = '#666666';
+
+	ctx.font = '18px Georgia';
+	ctx.fillStyle = 'orangered';
 	ctx.textBaseline = 'top';
-	ctx.fillText  (text, 0.5, 0);
-	ctx.fillText  (text, 1, 0);
-	ctx.fillText  (text, 1.5, 0);
-	ctx.fillText  (text, 2, 0);
+	ctx.fillText(text, 0.5, 0);
+	ctx.fillText(text, 1, 0);
+	ctx.fillText(text, 1.5, 0);
+	ctx.fillText(text, 2, 0);
+	ctx.fillText(text, 2.5, 0);
 
-	ctx.globalCompositeOperation = "source-over";
+	// ctx.globalCompositeOperation = "source-over";
 	ctx.font         = '18px Georgia';
 	ctx.fillStyle = 'orangered';
 	ctx.textBaseline = 'top';
-	ctx.fillText  (text, 0, 0);
+	ctx.fillText(text, 0, 0);
 
 }
 $(function(){
+	var lastSpan;
 	$('.underline').each(function(){
 		var words = $(this).text().split(" ");
 		var total = words.length;
 		$(this).empty();
 		for (index = 0; index < total; index ++){
-			var underlineSpan = $("<span class='und'></span> ").text(words[index]+' ');
-		    $(this).append(underlineSpan);
-			var canvasEL = $('<canvas></canvas>');
-			underlineSpan.append(canvasEL);
-			var canvas = canvasEL[0];
-			// this is 5px offset on width, wonder why
-			canvas.width = underlineSpan[0].offsetWidth + 4;
-			canvas.height = underlineSpan[0].offsetHeight;
-			drawText(canvas, words[index]+' ')
+
+			var underlineSpan = document.createElement("span");
+			underlineSpan.classList.add("und");
+			underlineSpan.innerHTML = words[index]+' ';
+			var canvas = document.createElement("canvas");
+			underlineSpan.appendChild(canvas);
+			$(this)[0].appendChild(underlineSpan);
+			if(lastSpan) {
+				//not the first span, now check if offsetTop changes from last time
+				if (underlineSpan.offsetTop === lastSpan.offsetTop) {
+					// offsetTop didn't change, no line break
+					drawText(lastSpan.childNodes[1], lastSpan.innerText);
+				} else {
+					// offsetTop changes, means the line breaks
+					lastSpan.classList.add("br");
+					drawText(lastSpan.childNodes[1], lastSpan.innerText.replace(/\s+/g, ''));
+				}
+			}
+			// drawText(canvas, underlineSpan.innerText);
+
+			lastSpan = underlineSpan;
 		}
+		// render the very last word
+		drawText(lastSpan.childNodes[1], lastSpan.innerText);
 	})
 })
