e304dfcf147529a06f57ce0300229b341ac44987
wentin <zhangwenting111@gmail.com>
2014-12-02 21:44:34 -0500

    for twitter discussion

--- js/underline.js
+++ js/underline.js
@@ -1,26 +1,34 @@
 
-function drawText(canvas, text){
+function drawText(canvas, text, fontFamily, fontSize, spanHeight){
 	var text = text;
 	var canvas = canvas;
 	var ctx = canvas.getContext('2d');
 
+	ctx.font = fontSize + ' ' + fontFamily;
+    var metrics = ctx.measureText('.');
+    var strokeWidth = metrics['width'];
+    console.log(strokeWidth);
 
-
-	ctx.font = '18px Georgia';
+	// ctx.font = '18px Georgia';
+	ctx.font = fontSize + ' ' + fontFamily;
     var metrics = ctx.measureText(text);
     canvas.width = metrics['width'];
-	canvas.height = canvas.offsetHeight;
+	canvas.height = spanHeight;
+
+    var posY = canvas.height * 0.95;
+    posY = Math.round(posY - 0.5) + 0.5;
 
 	ctx.strokeStyle = '#666666';
     ctx.lineWidth = 0.5;
-    var posY = 19.5;
 
 	ctx.moveTo(0, posY);
 	ctx.lineTo(canvas.width, posY);
 	ctx.stroke();
 
 	ctx.globalCompositeOperation = "destination-out";
-	ctx.font = '18px Georgia';
+	// ctx.font = '18px Georgia';
+	ctx.font = fontSize + ' ' + fontFamily;
+
 	ctx.fillStyle = 'orangered';
 	ctx.textBaseline = 'top';
 	ctx.fillText(text, -0.5, 0);
@@ -30,7 +38,9 @@ function drawText(canvas, text){
 	ctx.fillText(text, -2.5, 0);
 
 
-	ctx.font = '18px Georgia';
+	// ctx.font = '18px Georgia';
+	ctx.font = fontSize + ' ' + fontFamily;
+
 	ctx.fillStyle = 'orangered';
 	ctx.textBaseline = 'top';
 	ctx.fillText(text, 0.5, 0);
@@ -40,7 +50,8 @@ function drawText(canvas, text){
 	ctx.fillText(text, 2.5, 0);
 
 	// ctx.globalCompositeOperation = "source-over";
-	ctx.font         = '18px Georgia';
+	// ctx.font = '18px Georgia';
+	ctx.font = fontSize + ' ' + fontFamily;
 	ctx.fillStyle = 'orangered';
 	ctx.textBaseline = 'top';
 	ctx.fillText(text, 0, 0);
@@ -49,33 +60,39 @@ function drawText(canvas, text){
 $(function(){
 	var lastSpan;
 	$('.underline').each(function(){
+		console.log(window.getComputedStyle($(this)[0], null));
+		var fontFamily = window.getComputedStyle($(this)[0], null).getPropertyValue("font-family");
+		var fontSize = window.getComputedStyle($(this)[0], null).getPropertyValue("font-size");
 		var words = $(this).text().split(" ");
 		var total = words.length;
 		$(this).empty();
+		var spanHeight = 0;
 		for (index = 0; index < total; index ++){
-
 			var underlineSpan = document.createElement("span");
 			underlineSpan.classList.add("und");
 			underlineSpan.innerHTML = words[index]+' ';
 			var canvas = document.createElement("canvas");
 			underlineSpan.appendChild(canvas);
 			$(this)[0].appendChild(underlineSpan);
+			if (index == 0) {
+				spanHeight = underlineSpan.offsetHeight;
+			}
 			if(lastSpan) {
 				//not the first span, now check if offsetTop changes from last time
 				if (underlineSpan.offsetTop === lastSpan.offsetTop) {
 					// offsetTop didn't change, no line break
-					drawText(lastSpan.childNodes[1], lastSpan.innerText);
+					drawText(lastSpan.childNodes[1], lastSpan.innerText, fontFamily, fontSize, spanHeight);
 				} else {
 					// offsetTop changes, means the line breaks
 					lastSpan.classList.add("br");
-					drawText(lastSpan.childNodes[1], lastSpan.innerText.replace(/\s+/g, ''));
+					drawText(lastSpan.childNodes[1], lastSpan.innerText.replace(/\s+/g, ''), fontFamily, fontSize, spanHeight);
 				}
 			}
 			// drawText(canvas, underlineSpan.innerText);
-
+			// console.log(window.getComputedStyle(underlineSpan, null).getPropertyValue("height"));
 			lastSpan = underlineSpan;
 		}
 		// render the very last word
-		drawText(lastSpan.childNodes[1], lastSpan.innerText);
+		drawText(lastSpan.childNodes[1], lastSpan.innerText, fontFamily, fontSize, spanHeight);
 	})
 })
